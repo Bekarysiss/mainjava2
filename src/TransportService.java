6e47f462bbe61dd91cb2e3a9644e5fffad863f12
Bekarysiss <241483@astanait.edu.kz>
2025-01-09 21:16:17 +0500

    Update TransportService.java2

--- src/TransportService.java
+++ src/TransportService.java
@@ -1,58 +1,72 @@
-// TransportService Class
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class TransportService {
     private String serviceName;
-    private Bus[] buses;
-    private Commuter[] commuters;
-    private int busCount;
-    private int commuterCount;
+    private List<Bus> buses;
+    private List<Commuter> commuters;
 
-    // Constructor
     public TransportService(String serviceName) {
         this.serviceName = serviceName;
-        this.buses = new Bus[10]; // Assuming maximum of 10 buses
-        this.commuters = new Commuter[10]; // Assuming maximum of 10 commuters
-        this.busCount = 0;
-        this.commuterCount = 0;
+        this.buses = new ArrayList<>();
+        this.commuters = new ArrayList<>();
     }
 
-    // Getter and Setter methods
-    public String getServiceName() {
-        return serviceName;
-    }
-
-    public void setServiceName(String serviceName) {
-        this.serviceName = serviceName;
-    }
-
-    // Method to add a bus
     public void addBus(Bus bus) {
-        if (busCount < buses.length) {
-            buses[busCount++] = bus;
-        }
+        buses.add(bus);
     }
 
-    // Method to add a commuter
     public void addCommuter(Commuter commuter) {
-        if (commuterCount < commuters.length) {
-            commuters[commuterCount++] = commuter;
-        }
+        commuters.add(commuter);
     }
 
-    // Method to display service details
+    public List<Bus> filterBusesByCapacity(int minCapacity) {
+        return buses.stream()
+                .filter(bus -> bus.getCapacity() >= minCapacity)
+                .collect(Collectors.toList());
+    }
+
+    public List<Commuter> searchCommuterByName(String name) {
+        return commuters.stream()
+                .filter(commuter -> commuter.getName().equalsIgnoreCase(name))
+                .collect(Collectors.toList());
+    }
+
+    public List<Bus> sortBusesByCapacity() {
+        return buses.stream()
+                .sorted(Comparator.comparingInt(Bus::getCapacity))
+                .collect(Collectors.toList());
+    }
+
     public void displayServiceDetails() {
-        System.out.println("Service Name: " + serviceName);
-        System.out.println("Buses:");
-        for (int i = 0; i < busCount; i++) {
-            buses[i].displayBusDetails();
-        }
-        System.out.println("Commuters:");
-        for (int i = 0; i < commuterCount; i++) {
-            commuters[i].displayCommuterDetails();
-        }
+        System.out.println("Transport Service: " + serviceName);
+        System.out.println("\nBuses:");
+        buses.forEach(System.out::println);
+        System.out.println("\nCommuters:");
+        commuters.forEach(System.out::println);
     }
 
-    // Method to compare two TransportService objects
-    public boolean equals(TransportService other) {
-        return this.serviceName.equals(other.serviceName);
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        TransportService that = (TransportService) obj;
+        return serviceName.equals(that.serviceName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(serviceName);
+    }
+
+    @Override
+    public String toString() {
+        return "TransportService{" +
+                "serviceName='" + serviceName + '\'' +
+                ", buses=" + buses +
+                ", commuters=" + commuters +
+                '}';
     }
 }
