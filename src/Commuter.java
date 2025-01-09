713e68fdb95008dd1148e62693c0c9b2d0fa2310
Bekarysiss <241483@astanait.edu.kz>
2025-01-09 21:15:30 +0500

    Update Commuter.java2

--- src/Commuter.java
+++ src/Commuter.java
@@ -1,48 +1,24 @@
-// Commuter Class
-public class Commuter {
-    private String commuterId;
+public class Commuter extends TransportEntity {
     private String name;
     private int age;
 
-    // Constructor
-    public Commuter(String commuterId, String name, int age) {
-        this.commuterId = commuterId;
+    public Commuter(String id, String name, int age) {
+        super(id);
         this.name = name;
         this.age = age;
-    }
-
-    // Getter and Setter methods
-    public String getCommuterId() {
-        return commuterId;
-    }
-
-    public void setCommuterId(String commuterId) {
-        this.commuterId = commuterId;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public int getAge() {
         return age;
     }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    // Method to display commuter details
-    public void displayCommuterDetails() {
-        System.out.println("Commuter ID: " + commuterId + ", Name: " + name + ", Age: " + age);
-    }
-
-    // Method to compare two Commuter objects
-    public boolean equals(Commuter other) {
-        return this.commuterId.equals(other.commuterId);
+    @Override
+    public String toString() {
+        return super.toString() + ", Name: " + name + ", Age: " + age;
     }
 }
+
